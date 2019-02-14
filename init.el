11167ff3e2265def9846cbd7ba513d73806027cd
Jay Lee <jay@hanjaelee.com>
2019-02-15 01:16:23 +0900

    Add updates.

--- init.el
+++ init.el
@@ -562,10 +562,10 @@ Version 2016-06-19"
 ;; ibuffer-vc
 ;; ----------
 ;;
+(setq ibuffer-expert t) ;; disable prompt when deleting modified buffer.
 (define-key ibuffer-mode-map (kbd "M-o") 'ace-window) ;; override ibuffer mode map for M-o
 (add-hook 'ibuffer-hook (lambda ()
 			  (ibuffer-auto-mode 1) ;; keeps ibuffer list up to date
-			  (setq ibuffer-expert t) ;; disable prompt when deleting modified buffer.
 			  (setq ibuffer-show-empty-filter-groups nil) ;; don't show empty group
 			  (ibuffer-vc-set-filter-groups-by-vc-root)
 			  (ibuffer-do-sort-by-recency)))
