958b2a5203709e6a055d5399d8dba7d13bb19d80
Jay Lee <jay@hanjaelee.com>
2019-04-16 22:47:48 -0700

    Change cursor type.

--- init.el
+++ init.el
@@ -82,6 +82,7 @@
 (load-theme 'morning-star t)
 (custom-set-faces (if (not window-system) '(default ((t (:background "nil"))))))
 (set-fringe-mode '(0 . 0)) ;; remove fringe
+(setq-default cursor-type 'bar)
 
 ;; Dired
 ;; -----
