415fea22d22c794fde0a22c3b51e8bc41ff6f9af
Jay Lee <jay@hanjaelee.com>
2019-08-06 08:44:04 +0900

    Enable which func.

--- init.el
+++ init.el
@@ -58,7 +58,7 @@
 (show-paren-mode t)         ; Show parenthesis match
 (transient-mark-mode t)			; Highlight region
 ;; (global-hl-line-mode 1)
-;; (which-function-mode)
+(which-function-mode)
 (setq which-func-unknown "?")
 (set-face-attribute 'which-func nil :box '(:color "#000000") :bold t :foreground "#c98459")
 (setq inhibit-startup-message t)	; Inhibit startup message
