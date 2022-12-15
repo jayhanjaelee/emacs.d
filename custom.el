ee94a7bbacb274cc3ec0cfa748e9f163e49c9be0
Jay Lee <jay@hanjaelee.com>
2022-12-15 13:37:09 +0900

    Add Custom.

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(git-gutter dumb-jump realgud prodigy iedit yasnippet flycheck beacon minions undo-tree ripgrep wgrep exec-path-from-shell multi-term which-key ivy-rich ivy-hydra counsel-projectile ivy ibuffer-projectile eyebrowse projectile magit groovy-mode pip-requirements ssh-config-mode dockerfile-mode nginx-mode org-bullets yaml-mode markdown-mode web-mode go-mode swift-mode rust-mode ace-window dired-subtree dired-filter use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
